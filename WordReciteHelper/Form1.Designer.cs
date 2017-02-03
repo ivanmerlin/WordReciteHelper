e2ea6d8d4ae5524fe53ed3834753d513441462ab
ivanmerlin <fanlime24@qq.com>
2017-02-03 23:17:59 +0800

    basic display words

---
+++ WordReciteHelper/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WordLabel = new System.Windows.Forms.Label();
+            this.TransLabel = new System.Windows.Forms.Label();
+            this.RemoveBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WordLabel
+            // 
+            this.WordLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.WordLabel.AutoSize = true;
+            this.WordLabel.Font = new System.Drawing.Font("Consolas", 20F);
+            this.WordLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.WordLabel.Location = new System.Drawing.Point(287, 9);
+            this.WordLabel.Name = "WordLabel";
+            this.WordLabel.Size = new System.Drawing.Size(0, 32);
+            this.WordLabel.TabIndex = 0;
+            this.WordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TransLabel
+            // 
+            this.TransLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.TransLabel.Font = new System.Drawing.Font("宋体", 14F);
+            this.TransLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.TransLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.TransLabel.Location = new System.Drawing.Point(12, 61);
+            this.TransLabel.Name = "TransLabel";
+            this.TransLabel.Size = new System.Drawing.Size(597, 164);
+            this.TransLabel.TabIndex = 1;
+            this.TransLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // RemoveBtn
+            // 
+            this.RemoveBtn.Location = new System.Drawing.Point(631, 186);
+            this.RemoveBtn.Name = "RemoveBtn";
+            this.RemoveBtn.Size = new System.Drawing.Size(49, 48);
+            this.RemoveBtn.TabIndex = 2;
+            this.RemoveBtn.Text = "太熟悉";
+            this.RemoveBtn.UseVisualStyleBackColor = true;
+            this.RemoveBtn.Click += new System.EventHandler(this.RemoveBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
+            this.closeBtn.Location = new System.Drawing.Point(643, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(37, 28);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(631, 135);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(49, 45);
+            this.colorBtn.TabIndex = 4;
+            this.colorBtn.Text = "切换\r\n主题";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(680, 234);
+            this.ControlBox = false;
+            this.Controls.Add(this.colorBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.RemoveBtn);
+            this.Controls.Add(this.TransLabel);
+            this.Controls.Add(this.WordLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WordLabel;
+        private System.Windows.Forms.Label TransLabel;
+        private System.Windows.Forms.Button RemoveBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button colorBtn;
+    }
+}
+
---
+++ WordReciteHelper/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dictionary=new Dictionary<string, string>();
+        string[] keys;
+        FileStream fs ;
+        StreamWriter familiarWordWriter ;
+        int timeInterval = 3000;
+        public Form1()
+        {
+            loadGRE3000();
+            loadAndSortTheFamiliarWordList();
+            this.Opacity = 0.7;
+
+            InitializeComponent();
+
+            Thread displayWordThread = new Thread(startDisplayWords);
+            displayWordThread.IsBackground = true;
+            displayWordThread.Start();
+
+
+        }
+
+        private void loadAndSortTheFamiliarWordList()
+        {
+            fs = new FileStream("familiarWordList", FileMode.OpenOrCreate);
+            StreamReader reader = new StreamReader(fs);
+            string wordsToDelete = reader.ReadToEnd().ToString();
+            fs.Close();
+            string[] words = wordsToDelete.Split('\n');
+            HashSet<string> familiarWordList = new HashSet<string>();
+            foreach (string word in words)
+            {
+                familiarWordList.Add(word);
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary.Remove(word);
+                }
+            }
+            keys = dictionary.Keys.ToArray<string>();
+            if (familiarWordList.Count != words.Length)
+            {
+                fs = new FileStream("familiarWordList", FileMode.Create);
+                familiarWordWriter = new StreamWriter(fs);
+                foreach (string word in familiarWordList)
+                {
+                    familiarWordWriter.Write(word);
+                    familiarWordWriter.Write('\n');
+                }
+                familiarWordWriter.Flush();
+                familiarWordWriter.Close();
+            }
+            
+
+            fs = new FileStream("familiarWordList", FileMode.Append);
+            familiarWordWriter = new StreamWriter(fs);
+        }
+
+        private void updateWordLabels(string word)
+        {
+            WordLabel.Text = word;
+            TransLabel.Text = dictionary[word];
+        }
+       
+        private void startDisplayWords()
+        {
+            Thread.Sleep(10);
+            while (true)
+            {
+                
+                BeginInvoke((MethodInvoker) delegate {
+                    updateWordLabels(getNextWord());
+                });
+                Thread.Sleep(timeInterval);
+
+            }
+        }
+        private string getNextWord()
+        {
+            Random random = new Random();
+
+            int index = random.Next(dictionary.Count);
+            return keys[index];
+        }
+
+        private void loadGRE3000()
+        {
+            string wordListString = Resource1.WordList1;
+            string[] lines = wordListString.Split('\n');
+            Console.WriteLine(lines.Length);
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                if (lines[i].StartsWith("Q: "))
+                {
+                    string word = lines[i].Split(' ')[1];
+                    word=word.Remove(word.Length - 1);
+                    while (i < lines.Length && !lines[i].StartsWith("A: \r"))
+                    {
+                        ++i;
+                    }
+                    ++i;
+                    StringBuilder transOfWord = new StringBuilder(string.Empty);
+                    while (i < lines.Length && lines[i].Length > 1)
+                    {
+                        transOfWord.Append(lines[i]).Append('\n');
+                        ++i;
+                    }
+                    if (dictionary.Keys.Contains(word))
+                    {
+                        transOfWord.Append(dictionary[word]);
+                        dictionary[word] = transOfWord.ToString();
+                    }
+                    else
+                    {
+                        string transResult = transOfWord.ToString();
+                        dictionary.Add(word, transResult);
+                    }
+                }
+            }
+            keys = dictionary.Keys.ToArray<string>();
+            Console.WriteLine(dictionary.Count);
+        }
+
+        private void RemoveBtn_Click(object sender, EventArgs e)
+        {
+            string wordToDelete = WordLabel.Text;
+            if (dictionary.Keys.Contains(wordToDelete)) { 
+                dictionary.Remove(wordToDelete);
+                familiarWordWriter.Write(wordToDelete);
+                familiarWordWriter.Write("\n");
+                familiarWordWriter.Flush();
+                updateWordLabels(getNextWord());
+                keys = dictionary.Keys.ToArray<string>();
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        bool leftFlag = false;
+        Point mouseOff;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseOff = new Point(-e.X, -e.Y); //得到变量的值
+                leftFlag = true;                  //点击左键按下时标注为true;
+            }
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (leftFlag)
+            {
+                Point mouseSet = Control.MousePosition;
+                mouseSet.Offset(mouseOff.X, mouseOff.Y);  //设置移动后的位置
+                Location = mouseSet;
+            }
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (leftFlag)
+            {
+                leftFlag = false;//释放鼠标后标注为false;
+            }
+
+        }
+
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            if (this.WordLabel.ForeColor.Equals(System.Drawing.SystemColors.Control))
+            {
+                this.WordLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+                this.TransLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+                this.BackColor = System.Drawing.SystemColors.Control;
+            }
+            else
+            {
+                this.WordLabel.ForeColor = System.Drawing.SystemColors.Control;
+                this.TransLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+                this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            }
+               
+        }
+    }
+}
